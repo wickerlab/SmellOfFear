5ee9fd77238c87bbda7f155f2b5ab766d78f1bec
Hasnain <hasnaincheena.hc@gmail.com>
2019-06-06 10:17:44 +1200

    parametric and nonParametric tests

--- Randomisation Results.R
+++ Randomisation Results.R
@@ -7,6 +7,12 @@ filenames <- list.files(pattern=".csv")
 #create dataframes
 summaryDf <- data.frame(1,1,1,1,1)
 names(summaryDf) <- c("voc", "Randomised Mean Error", "Randomised Error SD", "Unrandomised Mean Error", "Unrandomised Error SD")
+resultsDf <- data.frame(1,1,1,1,1,1,1)
+names(resultsDf) <- c("voc", 
+                      "Two Sample T Test", "Two Sample Non-parametric Test",
+                      "Paired T Test", "Paired Non-parametric Test", 
+                      "One Sample T Test", "One Sample Non-parametric Test")
+
 
 pThreshold <- 0.05
 
@@ -27,24 +33,39 @@ for (filename in filenames){
   names(tempDf) <- c("voc", "Randomised Mean Error", "Randomised Error SD", "Unrandomised Mean Error", "Unrandomised Error SD")
   summaryDf <- rbind(summaryDf, tempDf)
   
-  
-  #T TESTS
+  #TWO SAMPLE TEST
   #two sample two tailed t-test - significant difference between the sample means 
   twoSampleTTest <- t.test(unrandomisedRMSE,randomisedRMSE)
-  
-  #paired two sample t test
-  twoSampleTTestPaired <- t.test(unrandomisedRMSE,randomisedRMSE, paired=TRUE)
-  
   #two sample non parametric Mann-Whitney U Test
   uTest <- wilcox.test(unrandomisedRMSE,randomisedRMSE)
   
+  #PAIRED TEST
+  #paired two sample t test
+  twoSampleTTestPaired <- t.test(unrandomisedRMSE,randomisedRMSE, paired=TRUE)
   #paired non parametric test
   uTestPaired <- wilcox.test(unrandomisedRMSE,randomisedRMSE,paired=TRUE)
+  
+  #ONE SAMPLE ONE TAIL TEST
+  meanUnrandomisedRMSE <- mean(unrandomisedRMSE)
+  #t test
+  oneSampleTTest <- t.test(randomisedRMSE, mu=meanUnrandomisedRMSE, alternative="greater")
+  #non parametric
+  oneSampleUTest <- wilcox.test(randomisedRMSE,mu=meanUnrandomisedRMSE, alternative="greater")
+  tempDf <- data.frame(voc, 
+                       twoSampleTTest$p.value, uTest$p.value,
+                       twoSampleTTestPaired$p.value, uTestPaired$p.value,
+                       oneSampleTTest$p.value, oneSampleUTest$p.value)
+  names(tempDf) <- c("voc", 
+                        "Two Sample T Test", "Two Sample Non-parametric Test",
+                        "Paired T Test", "Paired Non-parametric Test", 
+                        "One Sample T Test", "One Sample Non-parametric Test")
+  resultsDf <- rbind(resultsDf, tempDf)
   
 }
 
 #drop the first placeholder row of the dataframe
 summaryDf <- summaryDf[2:nrow(summaryDf),]
+resultsDf <- resultsDf[2:nrow(resultsDf),]
 
 #convert voc to factor
 summaryDf$voc = as.factor(summaryDf$voc)
